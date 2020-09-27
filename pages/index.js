b59afbfaf33ca80a81ad31a6deaa10a31f0a2ced
Kenny Chung <kenny.chung@appliedtechnology.se>
2020-09-27 18:37:14 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { EmptyState, Layout, Page } from "@shopify/polaris";
+import { ResourcePicker } from '@shopify/app-bridge-react';
+import store from 'store-js';
+
+function Index() {
+
+    const [modal, setModal] = useState({open: false});
+    const emptyState = !store.get('ids');
+
+    function handleSelection(resources) {
+        const idsFromResources = resources.selection.map((products) => products.id);
+        setModal({open: false});
+        store.set('ids', idsFromResources)
+        console.log('this is product ids', store.get('ids'))
+    }
+
+  return (
+    <Page>
+        <ResourcePicker 
+            resourceType="Product"
+            showVariant={false}
+            open={modal.open}
+            onCancel={() => setModal({open: false})}
+            onSelection={(resources) => handleSelection(resources)}
+        />
+        <Layout>
+            <EmptyState
+             heading="Manage your inventory transfers"
+             action={{content: 'Select Products',
+                    onAction: () => setModal({ open: true})
+                }}
+             image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg">
+                <p> Select Products</p>
+            </EmptyState>
+        </Layout>
+    </Page>
+  );
+}
+
+export default Index;
